71064b2f8955ef06ccb02e1ee6c8112ff2946458
PranavRasal <pranavcode19@gmail.com>
2026-06-27 23:43:06 +0530

    fix: update email HTML content in sendEmail function

--- helper/mailer.ts
+++ helper/mailer.ts
@@ -18,6 +18,8 @@ export const sendEmail = async ({email, emailType, userId}: any ) => {
 
 
 
+
+
        // Create a transporter using SMTP
         const transporter = nodemailer.createTransport({
         host: "smtp.example.com",
@@ -34,7 +36,7 @@ export const sendEmail = async ({email, emailType, userId}: any ) => {
     to: email, // list of recipients
     subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password", // subject line
     text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // HTML body
+    html: "<b>mail</b>", // HTML body
     };
 
     const mailResponse = await transporter.sendMail(mailOptions);
