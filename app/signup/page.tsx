b3cb1a9865f47a5766247c09b73ffefff06a40e8
PranavRasal <pranavcode19@gmail.com>
2026-07-01 02:13:21 +0530

    feat: implement user signup functionality with email verification and password hashing, add login and profile pages

---
+++ app/signup/page.tsx
@@ -0,0 +1,122 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import{toast} from 'react-hot-toast'
+import { useRouter } from 'next/navigation'
+import Link from 'next/link'
+
+export default function signupPage() {
+
+  const route = useRouter()
+  const [user , setUser] = useState({
+    username : "",
+    email : "",
+    password : ""
+  })
+  const[disabled , setDisabled] = useState(false)
+  const[loading , setLoading] = useState(false)
+
+  const onSingup = async()=>{
+    try {
+
+      setLoading(true)
+      const response = await axios.post("/api/users/signup",user)
+      toast.success("Signup successful!")
+      console.log("signup successful",response.data)
+      route.push("/login")
+
+
+
+    } catch (error : any) {
+      toast.error(error.message)
+      console.log("signup failed",error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+
+  useEffect(()=>{
+    if(user.username.length > 0 && user.email.length > 0 && user.password.length > 4){
+      setDisabled(false)
+    } else {
+      setDisabled(true)
+    }
+  },[user])
+
+  return (
+    <div className='relative min-h-screen flex items-center justify-center bg-white'>
+      <img
+        src='https://media.istockphoto.com/id/467367026/photo/perfect-sky-and-ocean.jpg?s=612x612&w=0&k=20&c=LZYFRxIhuKQom3l-kR3TLN7GikSJYJz9NRglQ3nngBw='
+        alt='background'
+        className='absolute inset-0 h-full w-full object-cover opacity-30'
+      />
+
+      <div className='relative z-10 w-full max-w-md rounded-3xl border-2 border-white/60 p-9 shadow-xl bg-transparent text-black space-y-4 backdrop-blur-lg'>
+        <h1 className='text-2xl font-bold text-center'>
+          {loading ? "processing 🔄️" : "Signup"}
+        </h1>
+        <br></br>
+
+        <div className='space-y-2'>
+          <label htmlFor='username' className='block font-medium'>Username</label>
+          <input
+            type='text'
+            id='username'
+            value={user.username}
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
+            className='border border-gray-300 rounded-md p-2 w-full'
+            placeholder='username'
+            required
+          />
+        </div>
+
+        <div className='space-y-2'>
+          <label htmlFor='email' className='block font-medium'>Email</label>
+          <input
+            type='email'
+            id='email'
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+            className='border border-gray-300 rounded-md p-2 w-full'
+            placeholder='email'
+            required
+          />
+        </div>
+
+        <div className='space-y-2'>
+          <label htmlFor='password' className='block font-medium'>Password</label>
+          <input
+            type='password'
+            id='password'
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            className='border border-gray-300 rounded-md p-2 w-full'
+            placeholder='password'
+            required
+          />
+        </div>
+        <br></br>
+
+        <button
+          onClick={onSingup}
+          className='w-full bg-blue-300 text-white px-4 py-2 rounded-md hover:bg-blue-600 disabled:opacity-50'
+          disabled={disabled}
+        >
+          {disabled ? "Fill all fields" : "Signup"}
+        </button>
+        
+          
+
+        <div className='text-center'>
+          <Link href='/login' className='text-blue-500 hover:underline'>
+            Already have an account? Login
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
